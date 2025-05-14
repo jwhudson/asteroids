1e69b69622ce2cf28626f4d5500408b4077f9d69
jwhudson <44750919+jwhudson@users.noreply.github.com>
2025-05-14 16:43:24 +0100

    added player class and updated screen

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
-    print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+    clock = pygame.time.Clock()
+    dt = 0
 
+    print("Starting Asteroids!")
+    
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
  
     while True:
+        # allows for when 'x' is pressed to exit the program
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
             
-            
         screen.fill("black")
+        player.draw(screen)
 
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
+        
 
 if __name__ == "__main__":
     main()
